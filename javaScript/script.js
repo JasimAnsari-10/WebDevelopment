0744ebb5402d5480efc92d068f583fd4224de5d1
Jasim Ansari <itsjasim.ansari@gmail.com>
2024-10-20 01:42:48 +0530

    js

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,12 +1,41 @@
-let name = "Jasim Ansari";
-console.log(name.length);
-console.log(name[0]);
-console.log(name[11]);
+// console.log("hello");
+// let name = "Jasim Ansari";
+// console.log(name.length);
+// console.log(name[0]);
+// console.log(name[11]);
+// console.log("apnacollege"+123);
 
 
 
+//traffic light
+// let color = "yellow";
+// if(color === "red"){
+//     console.log('stop');
+// }if(color === "yellow"){
+//     console.log('slow down');
+// }if(color === "green") {
+//     console.log('go');
+// }
 
 
+//practice
+// let sizeOfPopcorn = "L";
+// if (sizeOfPopcorn === "XL" ) {
+//     console.log("price is rs 250.");
+// }else if (sizeOfPopcorn === "L" ) {
+//     console.log("price is rs 200.");
+// }else if (sizeOfPopcorn === "M" ) {
+//     console.log("price is rs 100.");
+// }else if(sizeOfPopcorn === "S" ) { 
+//     console.log("price is rs 50.");
+// }else {
+//     console.log("invalid size");
+// }
 
-
-
+let str = 'apple';
+if((str[0] === 'a') && (str.length>3)){
+    console.log('string is good');
+}
+else {
+    console.log('string is not good');
+}
